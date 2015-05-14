62ab62cafedf19bd4227055fa9aa4d886b39a02f
Luiz Oliveira <ziuloliveira@gmail.com>
2015-05-13 22:36:34 -0300

    Fixing Event timer
    Signed-off-by: Luiz Oliveira <ziuloliveira@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from views.mainView import MainView
 
-from PyQt4.uic.Compiler.qtproxies import QtCore
+from pyqtgraph.Qt import QtGui, QtCore
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
@@ -10,6 +10,9 @@ import sys
 
 def main():
     main_view = MainView()
+    timer = QtCore.QTimer()
+    timer.timeout.connect(main_view.update)
+    timer.start(1)
     main_view.exec_()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
